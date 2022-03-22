7510efbc978270987a8b48c5625511fb69a07250
trankhoa2000 <khoa4755@gmail.com>
2022-03-23 01:54:48 +0700

    KhoaTM 01:54 23-03

--- FE/src/app/component/header/header.component.ts
+++ FE/src/app/component/header/header.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {TokenStorageService} from "../../service/token-storage.service";
+import {ShareService} from "../../service/share.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+@Injectable({
+  providedIn: 'root',
+})
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  idEmployee: string;
+  currentUser: string;
+  role: string;
+  isLoggedIn: boolean = false;
+
+  constructor(private tokenStorageService: TokenStorageService,
+              private shareService : ShareService) {
+    this.shareService.getClickEvent().subscribe(() => {
+      this.loadHeader();
+    })
+  }
+
+  loadHeader(): void {
+    if (this.tokenStorageService.getToken()) {
+      this.currentUser = this.tokenStorageService.getUser().username;
+      this.role = this.tokenStorageService.getUser().roles[0];
+      this.username = this.tokenStorageService.getUser().username;
+    }
+    this.isLoggedIn = this.username != null;
+    this.getUsernameAccount();
+  }
+
 
   ngOnInit(): void {
+    this.loadHeader();
+  }
+
+  logOut() {
+    this.tokenStorageService.signOut();
+    this.ngOnInit();
+  }
+
+  getUsernameAccount(){
+    if (this.tokenStorageService.getToken()) {
+      this.idEmployee = this.tokenStorageService.getUser().employee.employeeId;
+    }
   }
 
 }
