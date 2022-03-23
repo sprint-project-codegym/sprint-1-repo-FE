e132aa69d61c9b2d184fbd930804feb88321ad89
donghg201@gmail.com <donghg201@gmail.com>
2022-03-23 10:44:27 +0700

    DongVTH pull 23/03

--- FE/src/app/entity/IBuilding.ts
+++
@@ -1,9 +0,0 @@
-export interface IBuilding {
-  buildingId: string;
-  buildingName: string;
-  taxCode: string;
-  phoneNumber: string;
-  fax: string;
-  email: string;
-  address: string;
-}
--- FE/src/app/entity/IPosition.ts
+++
@@ -1,4 +0,0 @@
-export interface IPosition {
-  positionId: string;
-  positionName: string;
-}
