2a4cd2d12006653e657b9ee63a3007f27f537562
nguyendinhhunganh <nguyendinhhunganh99@gmail.com>
2022-03-23 22:30:22 +0700

    AnhNDH push code 23/3

--- FE/src/app/contract/contract-list/contract-list.component.ts
+++ FE/src/app/contract/contract-list/contract-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IContract} from "../../entity/IContract";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {ContractService} from "../../service/contract.service";
+import {Route, Router} from "@angular/router";
 
 @Component({
   selector: 'app-contract-list',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-list.component.scss']
 })
 export class ContractListComponent implements OnInit {
+  contractList: IContract[];
+  size = 2;
+  pageClicked = 0;
+  totalPages = 1;
+  pages = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private contractService: ContractService) {
   }
 
+  ngOnInit(): void {
+    this.onSubmit(0);
+  }
+
+  onFirst() {
+    this.pageClicked = 0;
+    this.onSubmit(this.pageClicked);
+  }
+
+  onPrevious() {
+    if (this.pageClicked > 0) {
+      this.pageClicked--;
+      this.onSubmit(this.pageClicked);
+    }
+  }
+
+  onSubmit(page) {
+    this.contractService.getAllContract(page, this.size).subscribe(
+      data => {
+        this.contractList = data['content'];
+        this.pageClicked = page;
+        this.totalPages = data.totalPages;
+        this.pages = Array.apply(null, {length: this.totalPages}).map(Number.call, Number);
+      }
+    );
+  }
+
+  onNext() {
+    if (this.pageClicked < this.totalPages - 1) {
+      this.pageClicked++;
+      this.onSubmit(this.pageClicked);
+    }
+  }
+
+  onLast() {
+    this.pageClicked = this.totalPages - 1;
+    this.onSubmit(this.pageClicked);
+  }
+
+  // search(page: Number) {
+  //   this.contractService.searchContractByIdAndCusName(this.idInput, this.cusNameInput).subscribe()
+  // }
 }
--- FE/src/app/service/contract.service.ts
+++ FE/src/app/service/contract.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContractService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  contUrrl = 'http://localhost:8080/list';
+  // getAllContract(): Observable<any> {
+  //   return this.httpClient.get(this.contUrrl);
+  // }
+  // get contract with pagination
+  getAllContract(page: number, size: number): Observable<any> {
+    return this.httpClient.get(this.contUrrl + '?page=' + page + '&size=' + size);
+  }
+  searchContractByIdAndCusName(id: String, cusName: String){
+    return this.httpClient.get(this.contUrrl + '/' + '?id=' + id + '&customerName=' + cusName);
+  }
 }
