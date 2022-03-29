f08070cda4ec435953c7753faccbca70ba7ca0c7
0971380599 <81423789+0971380599@users.noreply.github.com>
2022-03-29 20:25:11 +0700

    HungLM push code 29/3/2022

--- FE/src/app/ground/ground-delete/ground-delete.component.ts
+++ FE/src/app/ground/ground-delete/ground-delete.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CustomerService} from "../../service/customer.service";
+import {GroundService} from "../../service/ground.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-ground-delete',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ground-delete.component.scss']
 })
 export class GroundDeleteComponent implements OnInit {
+  @Input()
+  deleteId: string;
 
-  constructor() { }
+  @Output()
+  deleteComplete = new EventEmitter<boolean>();
+
+  constructor(
+    public groundService: GroundService,
+    public router: Router,
+    private toastrService: ToastrService
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  deleteGround() {
+    this.groundService.deleteGroundById(this.deleteId).subscribe(
+      data => {
+        document.getElementById('closeModal').click();
+        this.toastrService.success("Xóa thành công", "Thông báo")
+        this.deleteComplete.emit(true);
+      }, error => {
+        this.toastrService.error('Đã xảy ra lỗi', 'Vui lòng thử lại');
+        this.deleteComplete.emit(true);
+      }
+    );
   }
 
 }
--- FE/src/app/service/ground.service.ts
+++ FE/src/app/service/ground.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroundService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  groundUrl = 'http://localhost:8080/api/ground';
+  getAllGround(page: number, size:number): Observable<any> {
+    return this.httpClient.get(this.groundUrl +'/list'+'?page=' + page + "&size=" + size);
+  }
+
+  searchGround(id: string, type: string, page: number): Observable<any> {
+    return this.httpClient.get(this.groundUrl +'/list'+'?id=' + id + '&groundType=' + type + '&page=' + page);
+  }
+  // getAllLoaiXe(): Observable<any> {
+  //   return this.httpClient.get(this.groundUrl);
+  // }
+  // getXeById(id: any): Observable<any> {
+  //   return this.httpClient.get(this.groundUrl + '/' + id);
+  // }
+  // updateXe(obj: any): Observable<any> {
+  //   return this.httpClient.put(this.groundUrl + '/' + obj.id, obj);
+  // }
+  // deleteXe(id: any): Observable<any> {
+  //   return this.httpClient.delete(this.groundUrl + '/' + id);
+  // }
+
+  deleteGroundById(deleteId: string) {
+    return this.httpClient.delete(this.groundUrl + '/delete/' + deleteId);
+  }
 }
