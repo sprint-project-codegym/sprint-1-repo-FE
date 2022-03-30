b0fbe45bb0cbcea154e9ee441f9d5afd69a4e5ce
A0321I1 - Hồ Quốc Kiên <hquockien@gmail.com>
2022-03-30 10:42:11 +0700

    Kien update 30/03

--- FE/src/app/contract/contract.module.ts
+++ FE/src/app/contract/contract.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ContractRoutingModule } from './contract-routing.module';
-import { ContractListComponent } from './contract-list/contract-list.component';
-import { ContractEditComponent } from './contract-edit/contract-edit.component';
-import { ContractDeleteComponent } from './contract-delete/contract-delete.component';
+import {ContractRoutingModule} from './contract-routing.module';
+import {ContractListComponent} from './contract-list/contract-list.component';
+import {ContractEditComponent} from './contract-edit/contract-edit.component';
+import {ContractDeleteComponent} from './contract-delete/contract-delete.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ContractCreateComponent } from './contract-create/contract-create.component';
+import {ContractCreateComponent} from './contract-create/contract-create.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -14,19 +14,22 @@ import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-    declarations: [ContractListComponent, ContractEditComponent, ContractDeleteComponent, ContractCreateComponent],
-    exports: [
-        ContractCreateComponent
-    ],
-    imports: [
-        CommonModule,
-        ContractRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        MatDatepickerModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormsModule
-    ])
+  declarations: [ContractListComponent, ContractEditComponent, ContractDeleteComponent, ContractCreateComponent],
+  exports: [
+    ContractCreateComponent
+  ],
+  imports: [
+    CommonModule,
+    ContractRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'})
+  ]
+})
+
 export class ContractModule {
 }
