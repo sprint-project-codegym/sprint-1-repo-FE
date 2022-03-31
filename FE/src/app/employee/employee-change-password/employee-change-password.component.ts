d34514650e3adb9b3874512421071e7d57573b24
trankhoa2000 <khoa4755@gmail.com>
2022-03-31 17:49:26 +0700

    KhoaTM 31-03 18:00

--- FE/src/app/employee/employee-change-password/employee-change-password.component.ts
+++ FE/src/app/employee/employee-change-password/employee-change-password.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {PersonalInfoService} from '../../service/personal-info-service';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ToastrService} from 'ngx-toastr';
@@ -11,9 +11,10 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class EmployeeChangePasswordComponent implements OnInit {
   formAccount: FormGroup;
-  accountId = '12';
+  // accountId = '12';
+  id: number;
   userId;
-  private oldPass;
+  oldPass;
   notification: string;
   notificationPassNew: string;
 
@@ -21,17 +22,23 @@ export class EmployeeChangePasswordComponent implements OnInit {
     private router: Router,
     private personalInfoService: PersonalInfoService,
     private fb: FormBuilder,
-    private toast: ToastrService
+    private toast: ToastrService,
+    private activatedRoute: ActivatedRoute
   ) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+    });
   }
 
   ngOnInit(): void {
     // this.currentUser = this.token.getUser();
     // this.userId = this.currentUser.getAccount().getId();
-    this.userId = this.accountId;
+    // this.userId = this.accountId;
     this.formAccount = this.fb.group({
-      oldPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]],
-      newPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]],
+      // oldPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]],
+      // newPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]],
+      oldPassword: ['', [Validators.required, Validators.pattern(/^[-@.\/#&+\w\s]{3,15}$/)]],
+      newPassword: ['', [Validators.required, Validators.pattern(/^[-@.\/#&+\w\s]{3,15}$/)]],
       confirmNewPassword: ['', [Validators.required]]
     }, {validators: this.comparePassword});
   }
